16cc6cc7ae3dfa4baafa8597f348e81e55ab180f
Femolala <you@example.com0?>
2025-08-22 20:43:40 +0100

    testing git

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -5,7 +5,7 @@ export default function Home() {
 
 	return (
 		<>
-			<Layout breadcrumbTitle="About Us" mainCls="padding-0">
+			<Layout breadcrumbTitle="About Us" mainCls="padding-0 k">
 				<AboutUs />
 			</Layout>
 		</>
