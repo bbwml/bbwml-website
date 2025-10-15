9414198eb923f082274e4e89474bdd3028e4bd55
Femolala <you@example.com0?>
2025-10-15 10:54:34 +0100

    teesting build issues

--- components/email-template.jsx
+++ components/email-template.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export function EmailTemplate({ firstName }) {
   return (
     <div>
-      <h1>Welcome, {firstName}!</h1>
+      <h1>Welcome, {firstName}!.</h1>
     </div>
   );
 }
